5659f8fb48abf1f54e15dbf7d9ca8ff7a55fc4e9
Luciano Braga <lucianob525@gmail.com>
2020-07-21 15:39:30 -0300

    Implementando o método findByDepartment

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -43,9 +46,9 @@ public class SellerDaoJDBC implements SellerDao{
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		try {
-			st = conn.prepareStatement("SELECT seller.*,department.Name as DepName\r\n" 
-					+ "FROM seller INNER JOIN department\r\n" 
-					+ "ON seller.DepartmentId = department.Id\r\n" 
+			st = conn.prepareStatement("SELECT seller.*,department.Name as DepName " 
+					+ "FROM seller INNER JOIN department " 
+					+ "ON seller.DepartmentId = department.Id " 
 					+ "WHERE seller.Id = ?");
 			st.setInt(1, id);
 			rs = st.executeQuery();
@@ -63,7 +66,6 @@ public class SellerDaoJDBC implements SellerDao{
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
 		}
-		
 	}
 
 	private Seller intantiateSeller(ResultSet rs, Department dep) throws SQLException {
@@ -88,6 +90,42 @@ public class SellerDaoJDBC implements SellerDao{
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName\r\n" +"FROM seller INNER JOIN department " 
+					+ "ON seller.DepartmentId = department.Id " 
+					+ "WHERE DepartmentId = ? " 
+					+ "ORDER BY Name");
+			st.setInt(1, department.getId());
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+			//map para evitar que o mesmo departamento seja instanciado varias vezes
+			
+			while(rs.next()) {
+				Department dep = map.get(rs.getInt("DepartmentId")); //verifica se o departamento já existe
+				if(dep==null) { //se ele não existe vai ser instanciado
+				dep = instatiateDepartment(rs);
+				map.put(rs.getInt("DepartmentId"), dep);//salva o departamentId no map 
+				}
+				Seller obj = intantiateSeller(rs, dep);
+				list.add(obj);	
+			}
+			return list;
+		} catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 }
